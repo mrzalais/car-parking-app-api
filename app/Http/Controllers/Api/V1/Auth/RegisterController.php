3513f2ebffe7ef2df0247924263e9a0766427a4c
mrzalais <lejnieksedijs@gmail.com>
2023-06-21 11:35:24 +0300

    Add registration endpoint

---
+++ app/Http/Controllers/Api/V1/Auth/RegisterController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules\Password;
+use Symfony\Component\HttpFoundation\Response;
+
+class RegisterController extends Controller
+{
+    public function __invoke(Request $request): JsonResponse
+    {
+        $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'confirmed', Password::defaults()],
+        ]);
+
+        $user = User::create([
+            'name' => $request->input('name'),
+            'email' => $request->input('email'),
+            'password' => Hash::make($request->input('password')),
+        ]);
+
+        event(new Registered($user));
+
+        $device = substr($request->userAgent() ?? '', 0, 255);
+
+        return response()->json(
+            ['access_token' => $user->createToken($device)->plainTextToken],
+            Response::HTTP_CREATED
+        );
+    }
+}
