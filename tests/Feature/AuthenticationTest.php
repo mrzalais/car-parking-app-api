06e6e273a8191c80ad8453fcd350f5b6ef577db0
mrzalais <lejnieksedijs@gmail.com>
2023-06-22 17:45:19 +0300

    Add automated tests

---
+++ tests/Feature/AuthenticationTest.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class AuthenticationTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function testUserCanLoginWithCorrectCredentials(): void
+    {
+        /** @var User $user */
+        $user = User::factory()->create();
+
+        $response = $this->postJson('/api/v1/auth/login', [
+            'email'    => $user->email,
+            'password' => 'password',
+        ]);
+
+        $response->assertStatus(201);
+    }
+
+    public function testUserCannotLoginWithIncorrectCredentials(): void
+    {
+        /** @var User $user */
+        $user = User::factory()->create();
+
+        $response = $this->postJson('/api/v1/auth/login', [
+            'email'    => $user->email,
+            'password' => 'wrong_password',
+        ]);
+
+        $response->assertStatus(422);
+    }
+
+    public function testUserCanRegisterWithCorrectCredentials(): void
+    {
+        $response = $this->postJson('/api/v1/auth/register', [
+            'name'                  => 'John Doe',
+            'email'                 => 'john@example.com',
+            'password'              => 'password',
+            'password_confirmation' => 'password',
+        ]);
+
+        $response->assertStatus(201)
+            ->assertJsonStructure([
+                'access_token',
+            ]);
+
+        $this->assertDatabaseHas('users', [
+            'name'  => 'John Doe',
+            'email' => 'john@example.com',
+        ]);
+    }
+
+    public function testUserCannotRegisterWithIncorrectCredentials(): void
+    {
+        $response = $this->postJson('/api/v1/auth/register', [
+            'name'                  => 'John Doe',
+            'email'                 => 'john@example.com',
+            'password'              => 'password',
+            'password_confirmation' => 'wrong_password',
+        ]);
+
+        $response->assertStatus(422);
+
+        $this->assertDatabaseMissing('users', [
+            'name'  => 'John Doe',
+            'email' => 'john@example.com',
+        ]);
+    }
+}
